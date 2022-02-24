64fdd7ff5ef5fcb1e83c7c41423558b5e93ef514
LaysSthefany <93921690+LaysSthefany@users.noreply.github.com>
2022-02-24 00:25:05 -0300

    bd
    conexão com o banco de dados

--- paginas/admin.py
+++ paginas/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+
+from paginas.models import Alimento, Categoria, Fornecedor, Funcionario, TelefoneFornecedor, ValidadedeAlimento
+admin.site.register(Funcionario)
+admin.site.register(Alimento)
+admin.site.register(Fornecedor)
+admin.site.register(Categoria)
+admin.site.register(ValidadedeAlimento)
+admin.site.register(TelefoneFornecedor)
--- paginas/urls.py
+++ paginas/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import HomeView, RegisterUserView
+from .views import  HomeView, RegisterUserView
+from .views import FuncionarioCreateView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
     path('cadastro_de_usuario', RegisterUserView.as_view(), name='cadastro_de_usuario'),
-]
+
+
+
+    path('validar_cadastro_usuario', FuncionarioCreateView.as_view(), name='validar_cadastro_usuario'),
+]   
