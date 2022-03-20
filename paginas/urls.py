1d283f216de1b50b3139b64f6970402d546c0ac9
jose-ewerton <jose.ewerton@escolar.ifrn.edu.br>
2022-03-20 15:55:34 -0300

    att

--- paginas/urls.py
+++ paginas/urls.py
@@ -6,14 +6,8 @@ urlpatterns = [
     path('', HomeView.as_view(), name='login'),
     path('index', IndexView.as_view(), name = 'index'),
     path('login_de_usuario', LoginUserView.as_view(), name='login_de_usuario'),
-
     path('cadastro_de_funcionario', FuncionarioCreateView.as_view(), name='cadastro_de_funcionario'),
-
     path('registro_de_alimento', RegisterAlimentoView.as_view(), name='registro_de_alimento'),
-
     path('cadastro_de_usuario', RegisterUserView.as_view(), name='cadastro_de_usuario'),
-    
-    path ('registro', RegisterView.as_view(), name= 'registro'),
-    
-    
+    path ('registro', RegisterView.as_view(), name= 'registro'),    
     ]
